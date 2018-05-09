a133726fbe81eb609fbc799303877883db8381bb
Jacob Tompkins <term171013@gmail.com>
2018-05-09 12:13:50 +0800

    first draft of controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only:[:show]
   def def index
-
+    @restaurants = Restaurant.all
   end
 
   def show
@@ -8,16 +9,20 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-
+    Restaurant.create(restaurant_params)
   end
 
   private
 
   def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    r = Review.new(review_params)
+    r.restaurant = @restaurant
+    r.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
