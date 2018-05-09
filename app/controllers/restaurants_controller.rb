925d05423f5a2b81089944cd9ad0368bf19b231a
Jacob Tompkins <term171013@gmail.com>
2018-05-09 11:52:49 +0800

    Prepare rails app with external specs

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+  def def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def set_restaurant
+
+  end
 end
